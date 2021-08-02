3364461ac7ff2dba2b127a6c0b55ccc54206317c
EdoardoDini <edoardodini94@gmail.com>
2021-08-03 01:45:20 +0200

    Revert "Updated the project"
    This reverts commit ad15227a80bbbf6cfd191c8cc525537ee2db546b.

--- src/main/java/com/examples/spring/demo/DemoSpringTestingApplication.java
+++ src/main/java/com/examples/spring/demo/DemoSpringTestingApplication.java
@@ -1,13 +1,7 @@
 package com.examples.spring.demo;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Component;
-
-import com.examples.spring.demo.model.Employee;
-import com.examples.spring.demo.repositories.EmployeeRepository;
 
 @SpringBootApplication
 public class DemoSpringTestingApplication {
